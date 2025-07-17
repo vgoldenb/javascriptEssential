aae75a9d0fe8a9689abd8637144c33e97c74fa5e
vgoldenb <vitgold@yahoo.com>
2025-07-16 20:43:11 -0400

    added user exp

--- productSurvey/product_survey.js
+++ productSurvey/product_survey.js
@@ -9,6 +9,7 @@ function submitFeedback() {
     const designation = document.getElementById('designation').value;
     const productType = document.getElementById('productType').value;
     const feedback = document.getElementById('feedbackText').value;
+    const userExp = document.getElementById('userExperinceText').value;
 
     alert('Thank you for your valuable feedback');
 
@@ -19,6 +20,7 @@ function submitFeedback() {
     document.getElementById('userDesignation').innerHTML = designation;
     document.getElementById('userProductChoice').innerHTML = productType;
     document.getElementById('userFeedback').innerHTML = feedback;
+    document.getElementById('userExperince').innerHTML = userExp;
 
     document.getElementById('userInfo').style.display = 'block';
 }
